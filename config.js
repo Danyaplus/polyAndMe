7a314a962b315d2e33976ddcd41431215713be36
Danyaplus <141494329+Danyaplus@users.noreply.github.com>
2026-07-16 17:34:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // ВАЖНО: service_role key сюда вставлять НЕЛЬЗЯ.
 
 window.CHAT_CONFIG = {
-  supabaseUrl: "PASTE_SUPABASE_PROJECT_URL_HERE",
-  supabaseKey: "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE",
+  supabaseUrl: "https://toamoukqpuqizyqcumwj.supabase.co/rest/v1/",
+  supabaseKey: "sb_publishable_H0tzhOonw85h1tZX0HrKvw_u1Go2pfc",
   roomId: "e80f51a6-246a-40b1-93c7-e7a3ffa12535",
   maxFileSizeMb: 50
 };
